9a54a8219cb9f52a26b24168142071de1350f729
Sergej Nekrasov <130226127+SergejWinston@users.noreply.github.com>
2023-11-25 18:52:56 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/SergejWinston/BotFreelance
+cd BotFreelance
+cd bin
+source activate
+cd ..
+python3 -m pip install pytz
+python3 -m pip install pyTelegramBotAPI
+python3 -m pip install urllib3==1.26.6
+python3 -m pip install colorama
+python3 main.py
