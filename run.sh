dbedb6442542326527bc7e59d41f87b75ba9706b
Sergej Nekrasov <130226127+SergejWinston@users.noreply.github.com>
2023-11-25 19:11:08 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-cd bin
-source activate
-cd ..
-python3 -m pip install pytz
-python3 -m pip install pyTelegramBotAPI
-python3 -m pip install urllib3==1.26.6
-python3 -m pip install colorama
-python3 main.py
+
